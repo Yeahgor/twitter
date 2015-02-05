3f0726284963f5e378e3d8e4b3a8c66f6290d722
Georgiy Vylegzhanin <hmdfirm@gmail.com>
2015-02-05 03:06:24 +0000

    Heroku update

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
-ruby '2.0.0'
+ruby '2.1.1'
+
 gem 'rails', '4.1.0'
+gem 'bootstrap-sass', '3.2.0.0'
 
 group :development do
   gem 'sqlite3', '1.3.8'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
+
   root 'static_pages#home'
 
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   get 'help', to: 'static_pages#help'
+  get 'login', to: 'static_pages#login'
+  get 'signup', to: 'users#new'
 
-  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
