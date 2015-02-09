cded19ef16413dd19bf80b1e2b63ccd3cb1c8778
Georgiy Vylegzhanin <hmdfirm@gmail.com>
2015-02-09 10:40:07 +0000

    registartion

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
-  validates :email, presence: true, uniqueness: true
+  validates :name,  presence: true, length:{maximum: 50}
+                    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length:{ minimum: 6 }
 
-end
+  before_save {self.email.downcase!}
+  has_secure_password
+
+ end
